d91288ffbc1c0441fbb45a49f8c7e9618178a1e4
Gaurav <gauravp@zignuts.com>
2023-03-20 19:19:36 +0530

    added comment

--- routes/api.php
+++ routes/api.php
@@ -23,11 +23,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::controller(AuthController::class)->group(function(){
     Route::post('create','create');
     Route::post('login','login');
+
+    //user list is just for pagination practice.
     Route::get('userlist','userlist');
 });
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::controller(UserController::class)->prefix('user')->group(function(){
+        //show logged in user
         Route::get('show','show');
     });
 });
